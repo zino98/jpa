906c49227cbed13dac844a69a1458263d0e6367a
송진호 <songjinho776@gmail.com>
2022-03-04 23:10:43 +0900

    Item Entity

---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,48 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+    private int StockQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return StockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        StockQuantity = stockQuantity;
+    }
+}
