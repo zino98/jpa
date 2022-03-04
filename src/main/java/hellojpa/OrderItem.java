0c226cba9cf07d72ec2addb43f95b259fd0a1396
송진호 <songjinho776@gmail.com>
2022-03-04 23:13:04 +0900

    OrderItem Entity

---
+++ src/main/java/hellojpa/OrderItem.java
@@ -0,0 +1,62 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    private String id;
+
+    @Column(name = "ORDER_ID")
+    private String orderId;
+
+    @Column(name = "ITEM_ID")
+    private String itemId;
+
+    private int orderPrice;
+    private int count;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
