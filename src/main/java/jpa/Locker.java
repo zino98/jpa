27c32e04373510a17e2ca49629e9210b85fdcb14
송진호 <songjinho776@gmail.com>
2022-03-10 22:34:51 +0900

    Locker Entity

---
+++ src/main/java/jpa/Locker.java
@@ -0,0 +1,41 @@
+package jpa;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    @Column(name = "LOCKER_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+}
