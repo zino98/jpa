c53ff8907c471974cd88a631cf7fb65b8c7cf3fc
송진호 <songjinho776@gmail.com>
2022-03-10 22:51:22 +0900

    Product Entity

---
+++ src/main/java/jpa/Product.java
@@ -0,0 +1,42 @@
+package jpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Product {
+
+    @Id @GeneratedValue
+    @Column(name = "PRODUCT_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<MemberProduct> getMemberProducts() {
+        return memberProducts;
+    }
+
+    public void setMemberProducts(List<MemberProduct> memberProducts) {
+        this.memberProducts = memberProducts;
+    }
+}
