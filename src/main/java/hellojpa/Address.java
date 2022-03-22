4cfc3dbaba8f7f53e8073b0aac2f5012905d4eb4
송진호 <songjinho776@gmail.com>
2022-03-22 19:11:31 +0900

    Address Entity

---
+++ src/main/java/hellojpa/Address.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public Address() {
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+}
